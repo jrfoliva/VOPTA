925a37ce1bbdf90e686bc9eaea4e3f4fe0f731f9
jrfoliva <jrfoliva@gmail.com>
2022-07-19 12:15:51 -0300

    Versão DEMO 1.19

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// imports
+import http from "http";
+import express from "express";
+
+
+const port = 3000;
+const app = express();
+
+app.use(express.static('./public'));
+
+//rotas TODO!
+
+const server = http.createServer(app);
+server.listen(port, ()=> {
+    console.log('Server started at port: ', port);
+});
